bf70dc7ee6f64b0ff37cb79e40d1f6d6a51968d0
Silveira, Rodrigo <Rodrigo.Silveira@dematic.com>
2018-07-06 13:42:22 +0200

    dev/issue-28-boxplot replaced the population array with something more meaningful

--- src/app/boxplot-sparklines/boxplot-sparkline-simple/boxplot-sparkline-simple.component.ts
+++ src/app/boxplot-sparklines/boxplot-sparkline-simple/boxplot-sparkline-simple.component.ts
@@ -11,7 +11,7 @@ export class BoxplotSparklineSimpleComponent implements OnInit {
 	axisLineWidth = 1;
 	chartType = 'simple';
 	className = '';
-	height = '16';
+	height = '32';
 	highWhiskerColor  = 'black';
 	highWhiskerLineWidth = 1;
 	interQuartileRangeColor = 'black';
@@ -21,8 +21,27 @@ export class BoxplotSparklineSimpleComponent implements OnInit {
 	lowWhiskerLineWidth = 1;
 	medianColor = 'red';
 	medianLineWidth = 2;
-	population = JSON.stringify([1, 2, 3, 4]);
-	width = 64;
+	population = JSON.stringify([7,
+		17,
+		8,
+		15,
+		6,
+		7,
+		4,
+		14,
+		16,
+		16,
+		17,
+		6,
+		16,
+		20,
+		11,
+		16,
+		10,
+		19,
+		5,
+		18]);
+	width = 128;
 	constructor() { }
 
 	ngOnInit() {}
