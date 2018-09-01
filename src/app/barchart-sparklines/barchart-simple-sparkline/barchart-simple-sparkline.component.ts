f486c4b9178e035a7336245c9b41bd6f41475486
Rodrigo Silveira <Rodrigo.Silveira@madronesoft.com>
2018-09-01 14:08:16 -0700

    dev/issue-10-tooltips-barchart Resolved conflicts

--- src/app/barchart-sparklines/barchart-simple-sparkline/barchart-simple-sparkline.component.ts
+++ src/app/barchart-sparklines/barchart-simple-sparkline/barchart-simple-sparkline.component.ts
@@ -7,16 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BarchartSimpleSparklineComponent implements OnInit {
 
-	chartType = 'positive';
-	barHeights: string =  JSON.stringify([4, 3, 7, 8, 1, 4, 3, 2, 5, 3, 5, 8]);
-	minimumBarWidth = 3;
 	barGap = 6;
-	fillColorMinus = 'rgb(255,0,0)';
-	fillColorZero = 'rgb(0,255,0)';
-	fillColorPlus = 'rgb(0,0,255)';
-
+	barHeights: string =  JSON.stringify([4, 3, 7, 8, 1, 4, 3, 2, 5, 3, 5, 8]);
+	chartType = 'positive';
 	className = 'bar-chart';
+	fillColorMinus = 'rgb(255,0,0)';
+	fillColorPlus = 'rgb(0,0,255)';
+	fillColorZero = 'rgb(0,255,0)';
 	height = 32;
+	minimumBarWidth = 3;
 	width = 128;
 
 	constructor() { }
